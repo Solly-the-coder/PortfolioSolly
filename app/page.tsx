e18c4b2c48777cd4e7b7a887d1407a1b46ce030e
Solly <saleem.mitwalli@gmail.com>
2024-08-21 16:47:13 +0400

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+import Hero from "@/components/Hero";
+import { navItems } from "@/data";
+import Grid from "@/components/Grid";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import Image from "next/image";
+import { FaHome } from "react-icons/fa";
+import RecentProjects from "@/components/RecentProjects";
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems}/>
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Approach />
+        <Footer />
+      </div>
+    </main>
+  );
+}
